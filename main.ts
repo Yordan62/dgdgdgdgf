9a937c2ac55a6e1d329eaf00b82f9038a1fe0436
Yordan62 <118571602+Yordan62@users.noreply.github.com>
2024-03-15 19:31:40 +0000

    Update pxt.json, main.blocks, main.ts, main.py, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
- 
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles0, function (sprite, location) {
+    if (controller.A.isPressed()) {
+        timer.throttle("chop", 1000, function () {
+            wood += 1
+        })
+        guy.sayText(wood)
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (tiles.tileAtLocationEquals(guy.tilemapLocation(), sprites.builtin.forestTiles0)) {
+    	
+    }
+})
+let wood = 0
+let guy: Sprite = null
+tiles.setCurrentTilemap(tilemap`test`)
+guy = sprites.create(assets.image`Guy1`, SpriteKind.Player)
+guy.setScale(2, ScaleAnchor.Middle)
+controller.moveSprite(guy)
+wood = 0
+scene.cameraFollowSprite(guy)
