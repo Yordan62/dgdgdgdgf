9a84dd7d4ee999848b8a8c97b6be4c33b0dd46fa
Yordan62 <118571602+Yordan62@users.noreply.github.com>
2024-03-22 19:29:05 +0000

    Update pxt.json, main.blocks, main.ts, main.py, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Guy1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f . . . . . . . 
+. . . . f f . . f f . . . . . . 
+. . . f f e . e . f . . . . . . 
+. . . f 2 . . . 2 f . . . . f f 
+. . . f f 2 2 2 f . . . f f . . 
+f f f . . f f f . . f f . . . . 
+. . . f f f f f f f . . . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . f f . f f . . . . . . 
+. . . . . f . . . f . . . . . . 
+. . . . f f . . . . f . . . . . 
+. . . f f . . . . . f f . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "log":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . f f . . . . . . 
+. . . . . . . f e e f f . . . . 
+. . . . . . f e e e e e f . . . 
+. . . . . f e e e e e e f . . . 
+. . . f f e e e e e e f e f f . 
+. . f e f f e e e e f e e e e f 
+. f e e e e f e e f e e e e e f 
+. f e d d e f e f e e e e e e f 
+. f e d d e f f e e e e e e e f 
+. f e e e e f e f f e e e e f . 
+f e f f f f e e e e f e e f . . 
+f e d d e f e d d e f e f . . . 
+f e d d e f e d d e e f . . . . 
+f e e e e f e e e e f . . . . . 
+. f f f f f f f f f . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
